97587a3e1dc1119729773c2c538ca3f4daeaf915
jnelson <jnelson@C05C7C2689>
2019-01-23 09:55:31 +0000

    Add project files.

---
+++ Lesson39ForWithIf/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+class Program
+{
+    static void Main()
+    {
+        string s = "$7fd(*)3533%$m.?";
+        foreach (char c in s)
+        {
+            if (char.IsPunctuation(c))
+                WriteLine(c);
+        }
+
+        ReadKey();
+    }
+}
